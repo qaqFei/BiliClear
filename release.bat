2e9a0aa397d51716431f71e901a093f07df92149
qaq_fei <qaq_fei@163.com>
2024-09-06 17:47:26 +0800

    update release.bat

--- release.bat
+++ release.bat
@@ -8,9 +8,11 @@ python -m venv release-ven
 .\release-ven\Scripts\pyinstaller biliclear.py -i icon.ico
 .\release-ven\Scripts\pyinstaller biliclear_gui.py -i icon.ico
 
-xcopy .\dist\* .\ /s /e /y
+xcopy .\dist\biliclear\* .\ /s /e /y
+xcopy .\dist\biliclear_gui\* .\ /s /e /y
 
 rmdir /s /q release-ven
 rmdir /s /q build
 rmdir /s /q dist
 del biliclear.spec
+del biliclear_gui.spec
