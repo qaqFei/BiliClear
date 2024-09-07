6ef4e281962dab6107406532324d486dcba43deb
qaq_fei <qaq_fei@163.com>
2024-09-07 09:24:52 +0800

    change release.bat and biliclear_gui.py file name to ..._webui.py

--- release.bat
+++ release.bat
@@ -6,13 +6,13 @@ python -m venv release-ven
 .\release-ven\Scripts\pip install pyinstaller
 
 .\release-ven\Scripts\pyinstaller biliclear.py -i icon.ico
-.\release-ven\Scripts\pyinstaller biliclear_gui.py -i icon.ico
+.\release-ven\Scripts\pyinstaller biliclear_gui_webui.py -i icon.ico
 
 xcopy .\dist\biliclear\* .\ /s /e /y
-xcopy .\dist\biliclear_gui\* .\ /s /e /y
+xcopy .\dist\biliclear_gui_webui\* .\ /s /e /y
 
 rmdir /s /q release-ven
 rmdir /s /q build
 rmdir /s /q dist
 del biliclear.spec
-del biliclear_gui.spec
+del biliclear_gui_webui.spec
