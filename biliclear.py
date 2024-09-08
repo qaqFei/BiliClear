2a492e19cfc734d5ac7bbc6e849f056d3f38c08a
qaq_fei <qaq_fei@163.com>
2024-09-08 13:22:04 +0800

    fix bugs

--- biliclear.py
+++ biliclear.py
@@ -20,13 +20,11 @@ import gpt
 import syscmds
 from compatible_getpass import getpass
 
-sys.excepthook = lambda *args: [print("^C"), exec("raise SystemExit")] if KeyboardInterrupt in args[
-    0].mro() else sys.__excepthook__(*args)
+sys.excepthook = lambda *args: [print("^C"), exec("raise SystemExit")] if KeyboardInterrupt in args[0].mro() else sys.__excepthook__(*args)
 
 selfdir = dirname(sys.argv[0])
 if selfdir == "": selfdir = abspath(".")
 chdir(selfdir)
-
 
 def saveConfig():
     with open("./config.json", "w", encoding="utf-8") as f:
@@ -48,7 +46,6 @@ def saveConfig():
             "enable_check_lv2avatarat": enable_check_lv2avatarat,
             "enable_check_replyimage": enable_check_replyimage
         }, indent=4, ensure_ascii=False))
-
 
 def loadConfig():
     global sender_email, sender_password
@@ -78,7 +75,6 @@ def loadConfig():
     if reply_limit <= 20:
         reply_limit = 100
 
-
 def getCsrf(cookie: str):
     try:
         return re.findall(r"bili_jct=(.*?);", cookie)[0]
@@ -87,7 +83,6 @@ def getCsrf(cookie: str):
         print("请按回车键退出...")
         syscmds.pause()
         raise SystemExit
-
 
 def checkSmtpPassword():
     try:
@@ -98,24 +93,21 @@ def checkSmtpPassword():
     except smtplib.SMTPAuthenticationError:
         return False
 
-
 def getCookieFromUser():
     if "n" in input("\n是否使用二维码登录B站, 默认为是(y/n): ").lower():
         return getpass("Bilibili cookie: ")
     else:
         return biliauth.bilibiliAuth()
 
-
 def checkCookie():
     result = requests.get(
         "https://passport.bilibili.com/x/passport-login/web/cookie/info",
-        headers=headers,
-        data={
+        headers = headers,
+        data = {
             "csrf": csrf
         }
     ).json()
     return result["code"] == 0 and not result.get("data", {}).get("refresh", True)
-
 
 if not exists("./config.json"):
     sender_email = input("Report sender email: ")
@@ -195,10 +187,8 @@ print(f"加载完成, BiliClear将在{loaded_sleep_time}s后开始运行")
 time.sleep(loaded_sleep_time)
 syscmds.clearScreen()
 
-
 def _btyes2cv2im(btyes):
     return cv2.imdecode(np.frombuffer(btyes, np.uint8), cv2.IMREAD_COLOR)
-
 
 def _img_face(img):
     return not isinstance(
@@ -209,7 +199,6 @@ def _img_face(img):
         tuple
     )
 
-
 def getVideos():
     "获取推荐视频列表"
     return [
@@ -217,7 +206,6 @@ def getVideos():
         for i in requests.get(f"https://app.bilibili.com/x/v2/feed/index", headers=headers).json()["data"]["items"]
         if i.get("can_play", 0)
     ]
-
 
 def getReplys(avid: str | int):
     "获取评论"
@@ -228,7 +216,7 @@ def getReplys(avid: str | int):
         time.sleep(0.4)
         result = requests.get(
             f"https://api.bilibili.com/x/v2/reply?type=1&oid={avid}&nohot=1&pn={page}&ps=20",
-            headers=headers
+            headers = headers
         ).json()
         try:
             if not result["data"]["replies"]:
@@ -239,14 +227,12 @@ def getReplys(avid: str | int):
         page += 1
     return replies
 
-
 def isPorn(text: str):
     "判断评论是否为色情内容 (使用规则, rules.txt)"
     for rule in rules:
         if eval(rule):  # 一般来说, 只有rules.txt没有投毒, 就不会有安全问题
             return True, rule
     return False, None
-
 
 def reqBiliReportReply(data: dict, rule: str):
     "调用B站举报评论API"
@@ -276,7 +262,6 @@ def reqBiliReportReply(data: dict, rule: str):
         print("举报过于频繁, 等待60s")
         time.sleep(60)
         return reqBiliReportReply(data, rule)
-
 
 def reportReply(data: dict, r: str):
     "举报评论"
@@ -312,7 +297,6 @@ def reportReply(data: dict, r: str):
 
     print()  # next line
 
-
 def replyIsViolations(reply: dict):
     "判断评论是否违规, 返回: (是否违规, 违规原因) 如果没有违规, 返回 (False, None)"
     global enable_gpt
@@ -343,13 +327,15 @@ def replyIsViolations(reply: dict):
         print(f"lv.2和人脸检测, 结果: {isp}")
 
     if not isp and enable_check_replyimage and reply["member"]["level_info"]["current_level"] == 2:
-        images = [requests.get(i["img_src"], headers=headers).content for i in reply["content"]["pictures"]]
-        if any([bool(pyzbar.decode(np.ndarray(_btyes2cv2im(image)))) for image in images]):
-            isp, r = True, "lv.2, 检测到评论中包含二维码, 可疑"
-        print(f"lv.2和二维码检测, 结果: {isp}")
+        try:
+            images = [requests.get(i["img_src"], headers=headers).content for i in reply["content"]["pictures"]]
+            if any([bool(pyzbar.decode(np.ndarray(_btyes2cv2im(image)))) for image in images]):
+                isp, r = True, "lv.2, 检测到评论中包含二维码, 可疑"
+            print(f"lv.2和二维码检测, 结果: {isp}")
+        except Exception as e:
+            print("警告: 二维码检测时发生错误, 已跳过", repr(e))
 
     return isp, r
-
 
 def processReply(reply: dict):
     "处理评论并举报"
@@ -369,17 +355,14 @@ def processReply(reply: dict):
     violationsReplies = violationsReplies[:1500]
     return isp, r
 
-
 def videoIsViolations(avid: str | int):
     isp, r = False, None
 
     return isp, r
 
-
 def processVideo(avid: str | int):
     "处理视频并举报"
     isp, r = videoIsViolations(avid)
-
 
 def _setMethod():
     global method
@@ -403,10 +386,9 @@ def _setMethod():
 def bvid2avid(bvid: str):
     result = requests.get(
         f"https://api.bilibili.com/x/web-interface/view?bvid={bvid}",
-        headers=headers
+        headers = headers
     ).json()
     return result["data"]["aid"]
-
 
 videoCount = 0
 replyCount = 0
@@ -422,7 +404,6 @@ def _checkVideo(avid: str | int):
     processVideo(avid)
     for reply in getReplys(avid):
         processReply(reply)
-
 
 def checkNewVideos():
     global videoCount, replyCount, violationsReplyCount, checkedVideos
@@ -442,7 +423,6 @@ def checkNewVideos():
         checkedVideos = checkedVideos[:1500]
     time.sleep(1.25)
 
-
 def checkVideo(bvid: str):
     global videoCount, checkedVideos
 
@@ -452,7 +432,6 @@ def checkVideo(bvid: str):
     checkedVideos.insert(0, (avid, time.time()))
     checkedVideos = checkedVideos[:1500]
     time.sleep(1.25)
-
 
 def waitRiskControl(output: bool = True):
     global waitRiskControl_TimeRemaining, waitingRiskControl
@@ -470,7 +449,6 @@ def waitRiskControl(output: bool = True):
         else:
             time.sleep(0.005)
     waitingRiskControl = False
-
 
 if __name__ == "__main__":
     _setMethod()
